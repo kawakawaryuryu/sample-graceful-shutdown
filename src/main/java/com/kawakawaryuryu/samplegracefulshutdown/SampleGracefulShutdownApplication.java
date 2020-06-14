f0e48860acc945a13f0f0324198a7519f79060e6
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-06-15 01:42:46 +0900

    first commit

---
+++ src/main/java/com/kawakawaryuryu/samplegracefulshutdown/SampleGracefulShutdownApplication.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.samplegracefulshutdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGracefulShutdownApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleGracefulShutdownApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kawakawaryuryu/samplegracefulshutdown/SampleGracefulShutdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.samplegracefulshutdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGracefulShutdownApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
